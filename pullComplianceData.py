7ce2ca251d1d45645796a3ca4770c9236ce630d2
Stephen Beck <beckstep@amazon.com>
2018-01-11 00:14:32 -0500

    updates to pullComplianceData.py to produce a single datafile as output

--- pullComplianceData.py
+++ pullComplianceData.py
@@ -22,40 +22,31 @@ def get_compliance_data_by_account(accountId, bucketname, logger, assume_role_ar
     else:
         configsess = boto3.client('config')
     response = configsess.describe_compliance_by_resource(ResourceType='AWS::S3::Bucket')
-    non_compliant_resources = []
-    compliance_output = []
+    resource_ids = []
+    compliance_output = {}
     logger.info("Response is" + json.dumps(response) + ".")
     for resource in response['ComplianceByResources']:
-        resource_output = { 'AccountId': accountId, 'ResourceType': 'S3 Bucket', 'ResourceId': resource['ResourceId'], 'ComplianceStatus': resource['Compliance']['ComplianceType']}
-        compliance_output.append(resource_output)
-        if resource['Compliance']['ComplianceType'] == "NON_COMPLIANT":
-            non_compliant_resources.append(resource['ResourceId'])
+        resource_output = { 'BucketCompliance': resource['Compliance']['ComplianceType']}
+        compliance_output[resource['ResourceId']] = resource_output
+        resource_ids.append(resource['ResourceId'])
     
     logger.info("Compliance Output is " + json.dumps(compliance_output))
-    logger.info("Non-Compliant Resources is/are " + json.dumps(non_compliant_resources))
-#   key =  's3compliance/' + accountId + '/resourcecompliance.json'       
-#   object = s3res.Object(bucketname, key)
-#   object.put(Body=json.dumps(compliance_output))
     
-    non_compliant_output = []
-    for resourceid in non_compliant_resources:
+    detailed_resource_output = []
+    for resourceid in resource_ids:
         response = configsess.get_compliance_details_by_resource(ResourceType='AWS::S3::Bucket',ResourceId=resourceid)
         for ruleresult in response['EvaluationResults']:
-            rule_eval_output = { 'AccountId': accountId, 'ResourceType': 'S3 Bucket', 'ResourceId': resourceid }
+            rule_eval_output = { 'AccountId': accountId, 'ResourceType': 'S3 Bucket', 'BucketCompliance': compliance_output[resourceid]['BucketCompliance'], 'ResourceId': resourceid, 'RuleCompliance': ruleresult['ComplianceType'], 'ConfigRuleName': ruleresult['EvaluationResultIdentifier']['EvaluationResultQualifier']['ConfigRuleName'] }
+            rule_eval_output['ConfigRuleInvokedTime'] = str(ruleresult['ConfigRuleInvokedTime'])            
             if ruleresult['ComplianceType'] == 'NON_COMPLIANT':
-                rule_eval_output['ConfigRuleName'] = ruleresult['EvaluationResultIdentifier']['EvaluationResultQualifier']['ConfigRuleName']
                 if 'Annotation' in ruleresult:
                     rule_eval_output['Annotation'] = ruleresult['Annotation']
                 else:
                     rule_eval_output['Annotation'] = "No annotations for this rule are available"
-                rule_eval_output['ConfigRuleInvokedTime'] = str(ruleresult['ConfigRuleInvokedTime'])
-                non_compliant_output.append(rule_eval_output)
-    logger.info("Non-Compliant Resource by Rules Violated: " + json.dumps(non_compliant_output))
-#   key =  's3compliance/' + accountId + '/noncompliantresourcedetails.json'    
-#   object = s3res.Object(bucketname, key)
-#   object.put(Body=json.dumps(non_compliant_output))
+            detailed_resource_output.append(rule_eval_output)
+    logger.info("Detailed Resource Rule Compliance is " + json.dumps(detailed_resource_output))
     
-    return (compliance_output, non_compliant_output)
+    return (detailed_resource_output)
 
 
 def lambda_handler(event, context):
@@ -67,24 +58,16 @@ def lambda_handler(event, context):
     # Get main account and pull its data first
     callerInfo = sts.get_caller_identity()
     accountId = callerInfo['Account']
-    consolidated_resource_compliance_data = []
-    consolidated_non_compliant_detail_data = []
-    this_account_compliance_data, this_account_non_compliant_detail_data = get_compliance_data_by_account(accountId, bucketname, logger, "none")
-    consolidated_resource_compliance_data.extend(this_account_compliance_data)
-    consolidated_non_compliant_detail_data.extend(this_account_non_compliant_detail_data)    
+    consolidated_detail_data = []
+    this_account_detail_data = get_compliance_data_by_account(accountId, bucketname, logger, "none")
+    consolidated_detail_data.extend(this_account_detail_data)    
     # Then pull data for each managed account
     for managed_account in managed_accounts:
         assume_role_arn = assume_role_arn_template % (managed_account)
-        this_account_compliance_data, this_account_non_compliant_detail_data = get_compliance_data_by_account(managed_account, bucketname, logger, assume_role_arn)
-        consolidated_resource_compliance_data.extend(this_account_compliance_data)
-        consolidated_non_compliant_detail_data.extend(this_account_non_compliant_detail_data)
-        
-    logger.info("Consolidated compliance data is : " + json.dumps(consolidated_resource_compliance_data))
-    key =  's3compliance/resourcecompliance.json'       
-    object = s3res.Object(bucketname, key)
-    object.put(Body=json.dumps(consolidated_resource_compliance_data))
+        this_account_detail_data = get_compliance_data_by_account(managed_account, bucketname, logger, assume_role_arn)
+        consolidated_detail_data.extend(this_account_detail_data)
     
-    logger.info("Consolidated non_compliant detail data is : " + json.dumps(consolidated_non_compliant_detail_data))
-    key =  's3compliance/noncompliantresourcesdetails.json'    
+    logger.info("Consolidated detail data is : " + json.dumps(consolidated_detail_data))
+    key =  's3compliance/resourcescompliancetorules.json'    
     object = s3res.Object(bucketname, key)
-    object.put(Body=json.dumps(consolidated_non_compliant_detail_data))
+    object.put(Body=json.dumps(consolidated_detail_data))
