8f6057fa171ab7710183467b54d5924232472c0e
Stephen Beck <beckstep@amazon.com>
2018-01-18 10:17:44 -0500

    Implemented branches to prepare pdf and excel spreadsheet format in parallel in Step Functions and modified the sendEmailwithAttachment function to accept input from either use of branches (list input) or not (dict input)

--- sendEmailwithAttachment/sendEmailwithAttachment.py
+++ sendEmailwithAttachment/sendEmailwithAttachment.py
@@ -1,5 +1,7 @@
 import boto3
 import os
+import logging
+import json
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
@@ -13,8 +15,23 @@ myRecipients = os.environ['recipients']
 
 def lambda_handler(event, context):
     # get filename from event
-    s3bucketobjkey = event['s3bucketobjkey']
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    eventtype = type(event)
+
     
+    # Allow for event to be either dict as returned by preceding function if not using Step Function branches
+    # or a list if parallel branches are used as the preceding part of the flow.
+    # Wrap an event dict in an array to support both possibilities.
+
+    if eventtype is dict:
+        myInput = [ event ]
+        myInputType = 'dict'
+    else:
+        myInput = event
+        myInputType = 'list'
+        
+    logger.info("Event data is of type " + myInputType + ": " + json.dumps(event))
     # create raw email
     msg = MIMEMultipart()
     msg['Subject'] = 'AWSConfig Compliance Report'
@@ -23,18 +40,12 @@ def lambda_handler(event, context):
 
     part = MIMEText('Attached are AWSConfig Compliance reports')
     msg.attach(part)
-
-    response = s3_client.get_object(Bucket=myBucket, Key=s3bucketobjkey)
-    emailcontent = response['Body'].read()
-
-    part = MIMEApplication(emailcontent)
-    part.add_header('Content-Disposition', 'attachment', filename='ComplianceReport.pdf')
-    msg.attach(part)
-
     
-    ses.send_raw_email(RawMessage={
-                       'Data': msg.as_string(),
-                   }, 
-                   Source=msg['From'],
-                   Destinations=[msg['To']]
-                   )
+    for fileinfo in myInput:
+        filekey=fileinfo['s3bucketobjkey']
+        response = s3_client.get_object(Bucket=myBucket, Key=filekey)
+        emailcontent = response['Body'].read()
+
+        part = MIMEApplication(emailcontent)
+        part.add_header('Content-Disposition', 'attachment', filename=os.path.basename(filekey))
+        msg.attach(part)
